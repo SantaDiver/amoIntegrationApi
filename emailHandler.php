fe6ab43888334f38ba95ac73eca72ad587123c7c
Николай Савушкин <s02140294@stud.cs.msu.ru>
2017-04-21 00:11:11 +0300

    Обновленный emailHandler
    Добавлены проверки на домены и конкретные почты, которые обрабатывать не надо

--- emailHandler.php
+++ emailHandler.php
@@ -1,14 +1,51 @@
 <?php
 
+// print_r($_GET);
+if (!isset($_GET['uid'])) exit();
+
 require_once dirname(__FILE__) . '/amoIntegr.php';
 try {
-    $runAmo = new AmoAPI('amopresto', 'tim@presto-ps.com.ua', 'c0f0a68bfdc7dfe1c4412b0551637c6e');
+
+    $runAmo = new AmoAPI('icgr', '5179907@mail.ru', '1036ac2a6f0b3ca803e3dd2ab7e549ea');
     $sender = explode("<", $_POST['from']);
     $name = $sender[0];
     $email = trim($sender[1], ">");
     
+    if (ctype_space($email) || empty($email) || $email == 'support@amocrm.ru' || 
+        $name == 'support@amocrm.ru' || strpos($email, '@jivosite.com') !== false
+        || strpos($email, '@bisnescafe.ru') !== false)
+    {
+    	exit();
+    }
+
+    if (strpos($email, '@icgr.ru') !== false || strpos($email, '@amocrm.ru') !== false || 
+        $email == 'icgr@mail.ru' || $email == '5179907@mail.ru' || $email == 'nay82@mail.ru')
+    {
+        exit();
+    }
+
+    if (ctype_space($name) || empty($name))
+    {
+    	$name = $email;
+    }
+
+    $id = $_GET['uid'];//1018116
+
+    if (isset($_GET['mainp']) && isset($_GET['mains']));
+        $runAmo->setMainPipeline($_GET['mainp'], $_GET['mains']);
+    if (isset($_GET['repp']) && isset($_GET['reps']))
+        $runAmo->setRepPipeline($_GET['repp'], $_GET['reps']);
+    if (isset($_GET['did']))
+        $runAmo->setDepartmentID($_GET['did']);
+    
     $runAmo->setGenerateTasksForRec(false);
-    $runAmo->processData(array('NAME' => 'E-mail от клиента '.$name), array('EMAIL' => array('OTHER' => $email), 'NAME' => $name), "Заяка из email", 1018116);
+    
+    $runAmo->processData(
+        array('NAME' => 'E-mail от клиента '.$name), 
+        array('EMAIL' => array('OTHER' => $email), 'NAME' => $name, 'Источник добавления' => 'e-mail'), 
+        "Заяка из email", 
+        $id
+    );
 
 } catch(AmoException $e) {
 	echo 'AMO_EXCEPTION: '.$e->getMessage().PHP_EOL;
